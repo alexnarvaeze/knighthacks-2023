6120a21ea260e0c0422228e579788f75ae423d19
same-difference <dnmurillo@gmail.com>
2023-10-07 16:52:14 -0400

    fonts and grid

--- app/src/face.jsx
+++ app/src/face.jsx
@@ -2,9 +2,13 @@ import "./css/face.css"
 
 export default function Face() {
     return (
-        <div class="h-screen bg-gradient-to-b from-cyan-400 to-indigo-950 flex items-center space-y-10 join join-vertical">
-            <input type="text" id="linkedin-url" class="input input-bordered w-full max-w-xs join-item" />
-            <button class="btn btn-wide glass join-item">Wide</button>
+        <div class="h-screen grid grid-cols-1 row-auto bg-gradient-to-b from-cyan-400 to-indigo-950 space-y-10">
+            <h1 class="">Welcome to the Future of Networking</h1>
+            <h2 class="">Import your LinkedIn</h2>
+            <div class=" place-self-center  place-content-center">
+                <input type="text" id="linkedin-url" class=" input input-bordered rounded-full w-full max-w-xs " />
+                <button class=" btn btn-wide rounded-full glass">Sign Me Up!</button>
+            </div>
         </div>
     )
 }
