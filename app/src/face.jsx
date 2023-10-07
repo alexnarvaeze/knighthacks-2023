8003638ba9789f9be14d35299e4b0dc4c41bae1f
same-difference <dnmurillo@gmail.com>
2023-10-07 16:12:44 -0400

    center stuff

--- app/src/face.jsx
+++ app/src/face.jsx
@@ -2,9 +2,9 @@ import "./css/face.css"
 
 export default function Face() {
     return (
-        <div class="h-screen bg-gradient-to-b from-cyan-400 to-indigo-950 flex items-center space-y-10">
-            <input type="text" id="linkedin-url" class="input input-bordered w-full max-w-xs" />
-            <button class="btn btn-wide glass">Wide</button>
+        <div class="h-screen bg-gradient-to-b from-cyan-400 to-indigo-950 flex items-center space-y-10 join join-vertical">
+            <input type="text" id="linkedin-url" class="input input-bordered w-full max-w-xs join-item" />
+            <button class="btn btn-wide glass join-item">Wide</button>
         </div>
     )
 }
