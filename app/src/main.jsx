c25d4d8bd1c5575f5c29078a6a96e226f68111ab
sam <samanthaangel@gmail.com>
2023-10-08 10:14:29 -0400

    linked the front and back pages, and added transitions

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -5,12 +5,25 @@ import Face from "./face.jsx";
 import Back from "./back.jsx";
 import App from "./App.jsx";
 import "./css/index.css";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Face></Face>,
+  },
+  {
+    path: "back",
+    element: <Back></Back>,
+  },
+]);
+
+//Face is the default by router RouterProvider and back can be accessed through the button
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <TranscriptProvider>
-      <Face></Face>
-      <Back></Back>
+      <RouterProvider router={router} />
       <App></App>
     </TranscriptProvider>
   </React.StrictMode>
