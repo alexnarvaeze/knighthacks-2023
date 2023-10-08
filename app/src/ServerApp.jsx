4fc8d84bb7ac95fa314fdbacbc46d96e75f85ce0
same-difference <dnmurillo@gmail.com>
2023-10-07 23:05:22 -0400

    IT WORKS EUREKA

--- app/src/ServerApp.jsx
+++ app/src/ServerApp.jsx
@@ -1,31 +1,44 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 
-class ServerApp extends Component {
-    state = {
-        data: null
-    };
+// class ServerApp extends Component {
+//     state = {
+//         data: null
+//     };
 
-    componentDidMount() {
-        this.callBackendAPI()
-        .then(res => this.setState({ data: res.express }))
-        .catch(err => console.log(err));
-    }
+//     componentDidMount() {
+//         this.callBackendAPI()
+//         .then(res => this.setState({ data: res.express }))
+//         .catch(err => console.log(err));
+//     }
 
-    callBackendAPI = async () => {
-        const response = await fetch("/express_backend");
-        const body = await response.json();
+//     callBackendAPI = async () => {
+//         const response = await fetch("/express_backend");
+//         const body = await response.json();
+//         // console.log(body);
 
-        if (response.status !== 200) {
-            throw Error(body.message);
-        }
-        return body;
-    };
+//         if (response.status !== 200) {
+//             throw Error(body.message);
+//         }
+//         return body;
+//     };
 
-    render() {
-        return (
-            <p id="aaa">{this.state.data}</p>
-        );
-    }
+//     render() {
+//         return (
+//             <p id="aaa">{this.state.data}</p>
+//         );
+//     }
+// }
+
+// export default ServerApp
+
+function ServerApp() {
+    useEffect(() => {
+        fetch("http://localhost:5000/express_backend")
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
+
+    return <p></p>
 }
 
 export default ServerApp
--- app/src/server.js
+++ app/src/server.js
@@ -9,12 +9,16 @@ RouteA -> please do a
 
 */
 import express from "express"
+import cors from "cors"
+
 
 const app = express();
+app.use(cors())
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get("/express_backend", (req, res) => {
-    res.send({express: "EXPRESS X REACT NEW OTP"});
+    res.send({ express: "EXPRESS X REACT NEW OTP" });
 });
